da3985370381a379e0e2a5defa56b1a3c155474c
Neres <joaovneres@outlook.com>
2023-03-28 20:37:33 -0300

    First commit

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, NextFunction, Router } from "express";
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3333, () => console.log("Servidor ON"));
