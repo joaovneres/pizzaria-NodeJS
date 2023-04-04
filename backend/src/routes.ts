b26ded343096b274a7ac1a68964a2038430dded1
Neres <joaovneres@outlook.com>
2023-04-04 20:26:10 -0300

    Configurando retorno de erros e início de configuração do banco de dados

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,12 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, response } from "express";
 
 // Instanciando a constante router como do tipo Router
 const router = Router();
 
 // Criação de rota simples para a chamada de /teste que retorna um dado
 router.get("/teste", (req: Request, res: Response) => {
-  throw new Error("Erro ao fazer requisição");
-  //   return res.json({ nome: "João" });
+  // throw new Error("Erro ao fazer requisição");
+  return res.json({ nome: "João" });
 });
 
 // Exportação da constante router para acesso de outros arquivos
