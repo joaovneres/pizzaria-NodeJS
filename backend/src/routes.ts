ce3a101221f3ee5e2838770e05b710d925c3afba
Neres <joaovneres@outlook.com>
2023-04-03 22:15:20 -0300

    Configurando rotas

---
+++ backend/src/routes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from "express";
+
+// Instanciando a constante router como do tipo Router
+const router = Router();
+
+// Criação de rota simples para a chamada de /teste que retorna um dado
+router.get("/teste", (req: Request, res: Response) => {
+  throw new Error("Erro ao fazer requisição");
+  //   return res.json({ nome: "João" });
+});
+
+// Exportação da constante router para acesso de outros arquivos
+export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,16 @@
+// Importações
 import express, { Request, Response, NextFunction, Router } from "express";
+import "express-async-errors"; // Tratamento de erros do servidor
+import { router } from "./routes";
+
+// A aplicação será executada pelo servidor express
 const app = express();
 
+// Passando para o servidor que a comunicação de Request será através de JSON
+app.use(express.json());
 
+// Definindo que a aplicação utilizará as rotas fornecidas no arquivo routes.ts
+app.use(router);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// A aplicação rodará na porta 3333
 app.listen(3333, () => console.log("Servidor ON"));
