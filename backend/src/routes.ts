e963ab41be12779f219b58cb4cbda07256a89826
Neres <joaovneres@outlook.com>
2023-06-18 22:33:20 -0300

    fix errors

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -84,9 +84,3 @@ router.get("/finishorder", isAuthenticated, new FinishOrderController().handle);
 
 // Exportação da constante router para acesso de outros arquivos
 export { router };
-
-// Criação de rota simples para a chamada de /teste que retorna um dado
-router.get("/teste", (req: Request, res: Response) => {
-  // throw new Error("Erro ao fazer requisição");
-  return res.json({ nome: "João" });
-});
